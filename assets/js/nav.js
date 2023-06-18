f1a8a9c6f7aee3feea7cb0f9b595d5de1891d8e5
Hannah <hannah@hannahcodes.co.uk>
2023-06-18 12:45:48 +0100

    new dark mode/ side bar changes in the works

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -2,12 +2,26 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "260px";
     document.getElementById("main").style.marginLeft = "260px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.body.style.backgroundColor = "";
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "rgb(28, 29, 29)"; }
-    
+    document.body.style.backgroundColor = "inherit";
+  }
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+  var i;
+  
+  for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  } 
