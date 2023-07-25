5c5e8693fcb6e2cab6111d13bb0515051428bf95
Hannah <hannah@hannahcodes.co.uk>
2023-07-25 15:02:33 +0100

    woeking on new code tips page

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -93,3 +93,18 @@ window.onclick = function(event) {
     currentModal.style.display = "none";
   }
 }
+
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
